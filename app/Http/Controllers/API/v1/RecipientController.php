5b75ef0abcc5bd6d58d14a1a75f0d15fc0f35a0a
Abdurrahman Shofy Adianto <azophy@gmail.com>
2020-04-03 14:51:25 +0700

    DEBUG: get url rturned by env variabel

--- app/Http/Controllers/API/v1/RecipientController.php
+++ app/Http/Controllers/API/v1/RecipientController.php
@@ -22,6 +22,7 @@ class RecipientController extends Controller
         // Call external API
         $client = new GuzzleHttp\Client();
         $url = env('PELAPORAN_CITY_SUMMARY_API_URL','');
+        return [$url,null];
         $res = $client->get($url);
 
         if ($res->getStatusCode() != 200) {
