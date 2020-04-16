02887c13f17fb0e319f4869751ba8f0a7589074f
Randy <randy.hamzah.h@gmail.com>
2020-04-16 16:10:53 +0700

    update agency migration name

--- database/migrations/2020_04_16_081251_update_phone_number_in_agency_table.php
+++ database/migrations/2020_04_16_081251_update_phone_number_in_agency_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateNullableInAgencyTable extends Migration
+class UpdatePhoneNumberInAgencyTable extends Migration
 {
     /**
      * Run the migrations.
