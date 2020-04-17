5db3672929b78341ba334e092cdd6a2e5cb4bf28
Randy <randy.hamzah.h@gmail.com>
2020-04-17 11:23:59 +0700

    update typo

--- app/Http/Controllers/API/v1/NeedsController.php
+++ app/Http/Controllers/API/v1/NeedsController.php
@@ -11,26 +11,24 @@ class NeedsController extends Controller
 {
     public function store(Request $request)
     {
-        dd($request);
-
-        // $validator = Validator::make($request->all(), [
-        //     'agency_id' => 'required|numeric',
-        //     'applicant_id' => 'required|numeric',
-        //     'item' => 'string',
-        //     'brand' => 'string',
-        //     'quantity' => 'numeric',
-        //     'unit' => 'string',
-        //     'usage' => 'string',
-        //     'priority' => 'string'
-        // ]);
-        // if ($validator->fails()) {
-        //     return response()->json(['status' => 'fail', 'message' => $validator->errors()->all()]);
-        // } else {
-        //     // $model = new Needs();
-        //     // $model->fill($request->input());
-        //     // if ($model->save()) {
-        //     //     return $model;
-        //     // }
-        // }
+        $validator = Validator::make($request->all(), [
+            'agency_id' => 'required|numeric',
+            'applicant_id' => 'required|numeric',
+            'item' => 'string',
+            'brand' => 'string',
+            'quantity' => 'numeric',
+            'unit' => 'string',
+            'usage' => 'string',
+            'priority' => 'string'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status' => 'fail', 'message' => $validator->errors()->all()]);
+        } else {
+            $model = new Needs();
+            $model->fill($request->input());
+            if ($model->save()) {
+                return $model;
+            }
+        }
     }
 }
