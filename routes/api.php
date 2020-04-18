b0ac220509c2d32c89ce6bc312fdf8a2e0b9f05a
muhammad yusuf <muhammadd.yzf@gmail.com>
2020-04-18 17:26:35 +0700

    resolve indent problem

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,8 @@ Route::post('v1/login', 'API\v1\UsersController@authenticate');
 Route::post('v1/authenticate', 'API\v1\UsersController@authenticate');
 
 // Landing Page Registration
-Route::namespace('API\v1')->group(function () {
-  Route::prefix('v1/landing-page-registration')->group(function () {
+Route::namespace('API\v1')->group(function() {
+  Route::prefix('v1/landing-page-registration')->group(function() {
     //Landing Page Registration
     Route::post('/agency', 'AgencyController@store');
     Route::post('/applicant', 'ApplicantController@store');
@@ -33,48 +33,48 @@ Route::namespace('API\v1')->group(function () {
     Route::get('/areas/subdistricts', 'AreasController@getSubDistricts');
     Route::get('/areas/villages', 'AreasController@getVillages');
   });
-
-  Route::apiResource('v1/logistic-request', 'LogisticRequestController');
-  Route::apiResource('v1/master-faskes', 'MasterFaskesController');
-  Route::apiResource('v1/master-faskes-type', 'MasterFaskesTypeController');
+  
+    Route::apiResource('v1/logistic-request', 'LogisticRequestController');
+    Route::apiResource('v1/master-faskes', 'MasterFaskesController');
+    Route::apiResource('v1/master-faskes-type', 'MasterFaskesTypeController');
 });
 
 Route::namespace('API\v1')->middleware('auth:api')->group(function () {
-  // USER
-  Route::get('v1/users/me', 'UsersController@me');
-  Route::post('v1/users/register', 'UsersController@register');
+    // USER
+    Route::get('v1/users/me', 'UsersController@me');
+    Route::post('v1/users/register', 'UsersController@register');
 
-  // AREAS
-  Route::get('v1/areas/cities', 'AreasController@getCities');
-  Route::get('v1/areas/subdistricts', 'AreasController@getSubDistricts');
-  Route::get('v1/areas/villages', 'AreasController@getVillages');
+    // AREAS
+    Route::get('v1/areas/cities', 'AreasController@getCities');
+    Route::get('v1/areas/subdistricts', 'AreasController@getSubDistricts');
+    Route::get('v1/areas/villages', 'AreasController@getVillages');
 
-  // PRODUCTS
-  Route::get('v1/products', 'ProductsController@index');
-  Route::get('v1/products/{id}', 'ProductsController@show');
+    // PRODUCTS
+    Route::get('v1/products', 'ProductsController@index');
+    Route::get('v1/products/{id}', 'ProductsController@show');
 
-  // TRANSACTIONS
-  Route::prefix('v1/transactions')->group(function () {
-    Route::get('/summary', 'TransactionController@summary');
-    Route::get('/export', 'TransactionController@export');
-    Route::get('/{id}', 'TransactionController@show');
-    Route::put('/{id}', 'TransactionController@update');
-    Route::delete('/{id}', 'TransactionController@destroy');
-    Route::get('/', 'TransactionController@index');
-    Route::post('/', 'TransactionController@store');
-  });
+    // TRANSACTIONS
+    Route::prefix('v1/transactions')->group(function() {
+        Route::get('/summary', 'TransactionController@summary');
+        Route::get('/export', 'TransactionController@export');
+        Route::get('/{id}', 'TransactionController@show');
+        Route::put('/{id}', 'TransactionController@update');
+        Route::delete('/{id}', 'TransactionController@destroy');
+        Route::get('/', 'TransactionController@index');
+        Route::post('/', 'TransactionController@store');
+    });
 
-  Route::prefix('v1/recipients')->group(function () {
-    Route::get('/', 'RecipientController@index');
-    Route::get('/rdt-result-summary', 'RecipientController@summary_rdt_result');
-    Route::get('/summary', 'RecipientController@summary');
-    // need to be last so /summary wont be treated as city_code=summary
-    Route::get('/{city_code}', 'RecipientController@show');
-  });
+    Route::prefix('v1/recipients')->group(function() {
+      Route::get('/', 'RecipientController@index');
+      Route::get('/rdt-result-summary', 'RecipientController@summary_rdt_result');
+      Route::get('/summary', 'RecipientController@summary');
+      // need to be last so /summary wont be treated as city_code=summary
+      Route::get('/{city_code}', 'RecipientController@show');
+    });
 
-  Route::prefix('v1/recipients-faskes')->group(function () {
-    Route::get('/summary', 'RecipientFaskesController@summary');
-    Route::get('/export', 'RecipientFaskesController@export');
-    Route::get('/', 'RecipientFaskesController@index');
-  });
+    Route::prefix('v1/recipients-faskes')->group(function() {
+      Route::get('/summary', 'RecipientFaskesController@summary');
+      Route::get('/export', 'RecipientFaskesController@export');
+      Route::get('/', 'RecipientFaskesController@index');
+    });
 });
