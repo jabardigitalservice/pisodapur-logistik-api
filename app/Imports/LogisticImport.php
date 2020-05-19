64a56a9237eafeec4378843dfa6b73504fc9a5ab
muhammad yusuf <muhammadd.yzf@gmail.com>
2020-05-20 03:43:23 +0700

    implement shorthand ternary operator

--- app/Imports/LogisticImport.php
+++ app/Imports/LogisticImport.php
@@ -42,24 +42,24 @@ class LogisticImport extends Model
                 $agency = Agency::create([
                     'master_faskes_id' => $masterFaskesId,
                     'agency_type' => $masterFaskesTypeId,
-                    'agency_name' => $item['nama_instansi'] ? $item['nama_instansi'] : '-',
-                    'phone_number' => $item['nomor_telepon'] ? $item['nomor_telepon'] : '-',
-                    'location_district_code' => $districtCityId ? $districtCityId : '-',
-                    'location_subdistrict_code' => $subDistrictId ? $subDistrictId : '-',
-                    'location_village_code' => $villageId ? $villageId : '-',
-                    'location_address' => $item['alamat'] ? $item['alamat'] : '-',
+                    'agency_name' => $item['nama_instansi'] ?: '-',
+                    'phone_number' => $item['nomor_telepon'] ?: '-',
+                    'location_district_code' => $districtCityId ?: '-',
+                    'location_subdistrict_code' => $subDistrictId ?: '-',
+                    'location_village_code' => $villageId ?: '-',
+                    'location_address' => $item['alamat'] ?: '-',
                     'created_at' => $createdAt,
                     'updated_at' => $createdAt
                 ]);
 
                 $applicant = Applicant::create([
                     'agency_id' => $agency->id,
-                    'applicant_name' => $item['nama_pemohon'] ? $item['nama_pemohon'] : '-',
-                    'applicants_office' => $item['jabatan_pemohon'] ? $item['jabatan_pemohon'] : '-',
+                    'applicant_name' => $item['nama_pemohon'] ?: '-',
+                    'applicants_office' => $item['jabatan_pemohon'] ?: '-',
                     'file' => self::getFileUpload($item['ktp']),
-                    'email' => $item['email_pemohon'] ? $item['email_pemohon'] : '-',
-                    'primary_phone_number' => $item['nomor_telepon_pemohon_1'] ? $item['nomor_telepon_pemohon_1'] : '-',
-                    'secondary_phone_number' => $item['nomor_telepon_pemohon_2'] ? $item['nomor_telepon_pemohon_2'] : '-',
+                    'email' => $item['email_pemohon'] ?: '-',
+                    'primary_phone_number' => $item['nomor_telepon_pemohon_1'] ?: '-',
+                    'secondary_phone_number' => $item['nomor_telepon_pemohon_2'] ?: '-',
                     'verification_status' => $item['status_verifikasi'],
                     'source_data' => $item['source_data'],
                     'created_by' => $user->id,
@@ -127,28 +127,22 @@ class LogisticImport extends Model
     public static function getDistrictCity($data)
     {
         $city = City::where('kemendagri_kabupaten_nama', 'LIKE', "%{$data['kabupaten']}%")->first();
-        if ($city) {
-            return $city->kemendagri_kabupaten_kode;
-        }
-        return false;
+
+        return $city ? $city->kemendagri_kabupaten_kode : false;
     }
 
     public static function getSubDistrict($data)
     {
         $subDistrict = Subdistrict::where('kemendagri_kecamatan_nama', 'LIKE', "%{$data['kecamatan']}%")->first();
-        if ($subDistrict) {
-            return $subDistrict->kemendagri_kecamatan_kode;
-        }
-        return false;
+
+        return $subDistrict ? $subDistrict->kemendagri_kecamatan_kode : false;
     }
 
     public static function getVillage($data)
     {
         $village = Village::where('kemendagri_desa_nama', 'LIKE', "%{$data['desa']}%")->first();
-        if ($village) {
-            return $village->kemendagri_desa_kode;
-        }
-        return false;
+
+        return $village ? $village->kemendagri_desa_kode : false;
     }
 
     public static function getFileUpload($file)
@@ -163,14 +157,12 @@ class LogisticImport extends Model
         $result = [];
 
         foreach ($logisticItem as $item) {
-            if (isset($item['id_permohonan'])) {
-                if ($item['id_permohonan'] === $idPermohonan) {
-                    $productId = self::getProduct($item);
-                    $item['product_id'] = $productId;
-                    $unitId = self::getMasterUnit($item);
-                    $item['unit_id'] = $unitId;
-                    $result[] = $item;
-                }
+            if (isset($item['id_permohonan']) && $item['id_permohonan'] === $idPermohonan) {
+                $productId = self::getProduct($item);
+                $item['product_id'] = $productId;
+                $unitId = self::getMasterUnit($item);
+                $item['unit_id'] = $unitId;
+                $result[] = $item;
             }
         }
         return $result;
