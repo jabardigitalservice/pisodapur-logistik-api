572bb14b5cda3f7d2f56d4cf4614501fb1da445a
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-07-14 12:32:42 +0700

    Fix CRUD Realisasi oleh Admin + Konfigurasi Migrate

--- app/Http/Controllers/API/v1/LogisticRealizationItemController.php
+++ app/Http/Controllers/API/v1/LogisticRealizationItemController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\API\v1;
 
-use App\Needs;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\LogisticRealizationItems;
@@ -48,13 +47,11 @@ class LogisticRealizationItemController extends Controller
     public function add(Request $request)
     {
         $validator = Validator::make($request->all(), [             
-            'agency_id' => 'numeric',
-            'applicant_id' => 'numeric',
-            'product_id' => 'numeric',
-            'quantity' => 'numeric',
+            'agency_id' => 'numeric', 
+            'product_id' => 'numeric', 
             'unit_id' => 'numeric',
             'usage' => 'string',
-            'priority' => 'string', 
+            'priority' => 'string',
             'realization_quantity' => 'numeric',
             'realization_date' => 'date',
             'status' => 'string'
@@ -62,20 +59,14 @@ class LogisticRealizationItemController extends Controller
 
         if ($validator->fails()) {
             return response()->format(422, $validator->errors());
-        } elseif (!in_array($request->priority, Needs::STATUS)) {
-            return response()->json(['status' => 'fail', 'message' => 'priority value is not accepted']);
         } elseif (!in_array($request->status, LogisticRealizationItems::STATUS)) {
             return response()->json(['status' => 'fail', 'message' => 'verification_status_value_is_not_accepted']);
         } else {
             DB::beginTransaction();
-            try {  
-                $need = $this->needStore($request);
-                $request->request->add(['need_id' => $need->id]);
-
+            try {    
                 $realization = $this->realizationStore($request);
 
                 $response = array(
-                    'need' => $need,
                     'realization' => $realization
                 );
                 DB::commit();
@@ -106,28 +97,7 @@ class LogisticRealizationItemController extends Controller
             return response()->json(['errors' => $validator->errors()], 422);
         } else {
             $limit = $request->filled('limit') ? $request->input('limit') : 10;
-            $data = Needs::select(
-                'needs.id',
-                'needs.agency_id',
-                'needs.applicant_id',
-                'needs.product_id',
-                'needs.item',
-                'needs.brand',
-                'needs.quantity',
-                'needs.unit',
-                'needs.usage',
-                'needs.priority',
-                'needs.created_at',
-                'needs.updated_at',
-                'logistic_realization_items.need_id',
-                'logistic_realization_items.realization_quantity',
-                'logistic_realization_items.unit_id',
-                'logistic_realization_items.realization_date',
-                'logistic_realization_items.status',
-                'logistic_realization_items.realization_quantity',
-                'logistic_realization_items.created_by',
-                'logistic_realization_items.updated_by'
-            )
+            $data = LogisticRealizationItems::where('created_by', '999999')
             ->with([
                 'product' => function ($query) {
                     return $query->select(['id', 'name']);
@@ -135,18 +105,11 @@ class LogisticRealizationItemController extends Controller
                 'unit' => function ($query) {
                     return $query->select(['id', 'unit']);
                 }
-            ])
-            ->join('logistic_realization_items', 'logistic_realization_items.need_id', '=', 'needs.id', 'left')
+            ]) 
             ->whereNull('logistic_realization_items.deleted_at')
-            ->where('needs.created_by', 'admin')
-            ->orderBy('needs.id')
-            ->where('needs.agency_id', $request->agency_id)->paginate($limit);
-            $logisticItemSummary = Needs::where('needs.agency_id', $request->agency_id)->sum('quantity');
-            $data->getCollection()->transform(function ($item, $key) use ($logisticItemSummary) {
-                $item->status = !$item->status ? 'not_approved' : $item->status;
-                $item->logistic_item_summary = (int)$logisticItemSummary;
-                return $item;
-            });
+            ->orderBy('id')
+            ->where('agency_id', $request->agency_id)
+            ->paginate($limit);
         }
 
         return response()->format(200, 'success', $data);
@@ -161,11 +124,8 @@ class LogisticRealizationItemController extends Controller
     {
         $validator = Validator::make($request->all(), [    
             'agency_id' => 'numeric',  
-            'product_id' => 'numeric',
-            'quantity' => 'numeric',
-            'unit_id' => 'numeric',
-            'usage' => 'string',
-            'priority' => 'string',
+            'product_id' => 'numeric', 
+            'unit_id' => 'numeric', 
             'realization_quantity' => 'numeric',
             'realization_date' => 'date',
             'status' => 'string', 
@@ -174,19 +134,15 @@ class LogisticRealizationItemController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->format(422, $validator->errors());
-        } elseif (!in_array($request->priority, Needs::STATUS)) {
-            return response()->json(['status' => 'fail', 'message' => 'priority value is not accepted']);
+            return response()->format(422, $validator->errors()); 
         } elseif (!in_array($request->status, LogisticRealizationItems::STATUS)) {
             return response()->json(['status' => 'fail', 'message' => 'verification_status_value_is_not_accepted']);
         } else {
             DB::beginTransaction();
-            try {  
-                $need = $this->needUpdate($request, $id);
+            try {   
                 $realization = $this->realizationUpdate($request, $id);
 
-                $response = array(
-                    'need' => $need,
+                $response = array( 
                     'realization' => $realization
                 );
                 DB::commit();
@@ -207,34 +163,14 @@ class LogisticRealizationItemController extends Controller
     public function destroy($id)
     {
         DB::beginTransaction();
-        try {  
-            $deleteNeed = Needs::where('id', $id)->delete();
-            $deleteRealization = LogisticRealizationItems::where('need_id', $id)->delete();
+        try {   
+            $deleteRealization = LogisticRealizationItems::where('id', $id)->delete();
             DB::commit();
         } catch (\Exception $exception) {
             DB::rollBack();
             return response()->format(400, $exception->getMessage());
         }
         return response()->format(200, 'success', ['id' => $id]);
-    }
-    
-    public function needStore($request)
-    { 
-        $need = Needs::create(
-            [
-                'agency_id' => $request->input('agency_id'),
-                'applicant_id' => $request->input('applicant_id'),
-                'product_id' => $request->input('product_id'),
-                'brand' => $request->input('brand'),
-                'quantity' => $request->input('quantity'),
-                'unit' => $request->input('unit_id'),
-                'usage' => $request->input('usage'),
-                'priority' => $request->input('priority'),
-                'created_by' => 'admin'
-            ]
-        );
-
-        return $need;
     }
 
     public function realizationStore($request)
@@ -248,34 +184,17 @@ class LogisticRealizationItemController extends Controller
                 'unit_id' => $request->input('unit_id'),
                 'realization_date' => $request->input('realization_date'),
                 'status' => $request->input('status'),
+                'created_by' => 999999
             ]
         );
 
         return $realization;
     }
-        
-    public function needUpdate($request, $id)
-    { 
-        $need = Needs::where('id', $id)->update(
-            [
-                'agency_id' => $request->input('agency_id'), 
-                'product_id' => $request->input('product_id'),
-                'brand' => $request->input('brand'),
-                'quantity' => $request->input('quantity'),
-                'unit' => $request->input('unit_id'),
-                'usage' => $request->input('usage'),
-                'priority' => $request->input('priority'),
-                'created_by' => 'admin'
-            ]
-        );
-
-        return $need;
-    }
 
     public function realizationUpdate($request, $id)
     {
         $model = new LogisticRealizationItems();
-        $findOne = LogisticRealizationItems::where('need_id', $id)->orderBy('created_at', 'desc')->first();
+        $findOne = LogisticRealizationItems::find($id);
         $model->fill(
             [ 
                 'need_id' => $id,
@@ -285,6 +204,7 @@ class LogisticRealizationItemController extends Controller
                 'unit_id' => $request->input('unit_id'),
                 'realization_date' => $request->input('realization_date'),
                 'status' => $request->input('status'),
+                'created_by' => 999999
             ]
         );
         $model->save();
