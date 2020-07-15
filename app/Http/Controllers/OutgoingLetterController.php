650f220550a9ba9a68b49a1b774fbd4113f89734
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-07-15 11:36:38 +0700

    Create Controller and Model for Outgoing Letters

---
+++ app/Http/Controllers/OutgoingLetterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OutgoingLetter;
+use Illuminate\Http\Request;
+
+class OutgoingLetterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\OutgoingLetter  $outgoingLetter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OutgoingLetter $outgoingLetter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\OutgoingLetter  $outgoingLetter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OutgoingLetter $outgoingLetter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\OutgoingLetter  $outgoingLetter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OutgoingLetter $outgoingLetter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\OutgoingLetter  $outgoingLetter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OutgoingLetter $outgoingLetter)
+    {
+        //
+    }
+}
