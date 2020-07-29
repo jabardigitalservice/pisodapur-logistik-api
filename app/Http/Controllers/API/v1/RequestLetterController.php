ab26511db9b3d4cc72bb7783e9ca8c235762c9ac
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-07-29 16:46:50 +0700

    Code Revision to conditioning parameter letter number if exist

--- app/Http/Controllers/API/v1/RequestLetterController.php
+++ app/Http/Controllers/API/v1/RequestLetterController.php
@@ -173,7 +173,11 @@ class RequestLetterController extends Controller
 
         try { 
             $list = Applicant::select('id', 'application_letter_number')
-                ->where('application_letter_number', 'LIKE', "%{$request->input('application_letter_number')}%")
+                ->where(function ($query) use ($request) {
+                    if ($request->filled('application_letter_number')) {
+                        $query->where('application_letter_number', 'LIKE', "%{$request->input('application_letter_number')}%");
+                    }
+                }) 
                 ->where('is_deleted', '!=', 1)
                 ->where('application_letter_number', '!=', '')
                 ->get();
