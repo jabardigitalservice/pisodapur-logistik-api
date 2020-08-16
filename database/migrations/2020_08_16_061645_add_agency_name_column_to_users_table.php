c40517f176ac4403f05c14e9315f28f04606f964
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-08-16 13:39:59 +0700

    Set Default Agency Name to Migrate Agency Name

--- database/migrations/2020_08_16_061645_add_agency_name_column_to_users_table.php
+++ database/migrations/2020_08_16_061645_add_agency_name_column_to_users_table.php
@@ -16,6 +16,12 @@ class AddAgencyNameColumnToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('agency_name')->nullable()->after('roles');
         });
+
+        DB::table('users')->where('username', 'like', '%gtlog%')->update(['agency_name' => 'Divisi Logistik']);
+        DB::table('users')->where('name', 'like', '%log%')->update(['agency_name' => 'Divisi Logistik']);
+        DB::table('users')->where('username', 'like', '%dinkes%')->update(['agency_name' => 'Dinas Kesehatan']);
+        DB::table('users')->where('name', 'like', '%Jabar Bergerak%')->update(['agency_name' => 'Jabar Bergerak']);
+        DB::table('users')->where('username', 'like', '%indag%')->update(['agency_name' => 'Dinas Perindustrian dan Perdagangan']);
     }
 
     /**
