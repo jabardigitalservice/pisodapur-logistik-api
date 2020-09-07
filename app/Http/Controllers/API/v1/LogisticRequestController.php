01c79fe362a94dcd57c1b7d80bce5679ed1c8f86
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-09-07 14:40:02 +0700

    Requests Total Statistic

--- app/Http/Controllers/API/v1/LogisticRequestController.php
+++ app/Http/Controllers/API/v1/LogisticRequestController.php
@@ -439,11 +439,6 @@ class LogisticRequestController extends Controller
         $endDate = $request->filled('end_date') ? $request->input('end_date') . ' 23:59:59' : date('Y-m-d H:i:s');
 
         try {
-            $total = Applicant::Select('applicants.id')
-            ->where('is_deleted', '!=' , 1)
-            ->whereBetween('updated_at', [$startDate, $endDate])
-            ->count();
-
             $lastUpdate = Applicant::Select('applicants.updated_at') 
             ->where('is_deleted', '!=' , 1) 
             ->orderBy('updated_at', 'desc')
@@ -461,27 +456,48 @@ class LogisticRequestController extends Controller
             ->whereBetween('updated_at', [$startDate, $endDate])
             ->count();
 
+            $totalUnverified = Applicant::Select('applicants.id') 
+            ->where('approval_status', Applicant::STATUS_NOT_APPROVED) 
+            ->where('verification_status', Applicant::STATUS_NOT_VERIFIED) 
+            ->where('is_deleted', '!=' , 1)
+            ->whereBetween('updated_at', [$startDate, $endDate])
+            ->count();
+
             $totalApproved = Applicant::Select('applicants.id') 
-            ->where('verification_status', Applicant::STATUS_APPROVED) 
+            ->where('approval_status', Applicant::STATUS_APPROVED) 
+            ->where('verification_status', Applicant::STATUS_VERIFIED)
             ->where('is_deleted', '!=' , 1)
             ->whereBetween('updated_at', [$startDate, $endDate])
             ->count();
 
             $totalVerified = Applicant::Select('applicants.id') 
+            ->where('approval_status', Applicant::STATUS_NOT_APPROVED) 
             ->where('verification_status', Applicant::STATUS_VERIFIED) 
             ->where('is_deleted', '!=' , 1)
             ->whereBetween('updated_at', [$startDate, $endDate])
             ->count();
 
-            $totalRejected = Applicant::Select('applicants.id') 
+            $totalVerificationRejected = Applicant::Select('applicants.id') 
+            ->where('approval_status', Applicant::STATUS_NOT_APPROVED) 
             ->where('verification_status', Applicant::STATUS_REJECTED)
             ->where('is_deleted', '!=' , 1)
             ->whereBetween('updated_at', [$startDate, $endDate])
             ->count();
 
+            $totalApprovalRejected = Applicant::Select('applicants.id') 
+            ->where('approval_status', Applicant::STATUS_REJECTED)
+            ->where('verification_status', Applicant::STATUS_VERIFIED)
+            ->where('is_deleted', '!=' , 1)
+            ->whereBetween('updated_at', [$startDate, $endDate])
+            ->count();
+
+            $totalRejected = $totalVerificationRejected + $totalApprovalRejected;
+            $total = $totalUnverified + $totalVerified + $totalApproved + $totalRejected;
+
             $data = [
                 'total_request' => $total,
                 'total_approved' => $totalApproved,
+                'total_unverified' => $totalUnverified,
                 'total_verified' => $totalVerified,
                 'total_rejected' => $totalRejected,
                 'total_pikobar' => $totalPikobar,
