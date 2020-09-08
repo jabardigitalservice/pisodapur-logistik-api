ca41f90c378e8e98a244f53aff10aec4707b5a04
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-09-08 14:06:52 +0700

    Fix Dashboard Summary 'unauthenticated'

--- app/Http/Middleware/AuthKey.php
+++ app/Http/Middleware/AuthKey.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use JWTAuth;
 
 class AuthKey
 {
@@ -15,10 +16,28 @@ class AuthKey
      */
     public function handle($request, Closure $next)
     {
+        $JWTtoken = JWTAuth::getToken();
         $token = $request->header('Api-Key');
         $authKey = \App\AuthKey::whereToken($token)->first();
         if (!isset($authKey)) {
-            return response()->json(['message' => 'Unauthenticated'], 401);
+            if (!$JWTtoken) {
+                return response()->json(['message' => 'Unauthenticated'], 401);
+            } else {
+                try {
+                    if (!$user = JWTAuth::parseToken()->authenticate()) {
+                        return response()->format(404, 'user_not_found');
+                    }
+                    $request->merge(array("authenticated_user_id" => $user->id));
+                } catch (TokenExpiredException $e) {
+                    $token = $request->token;
+                    $refreshedToken = JWTAuth::refresh($token);
+                    return response()->format(200, "token_expired", ["new_token" => $refreshedToken]);
+                } catch (JWTException $e) {
+                    return response()->format(422, $e->getMessage());
+                } catch (Exception $exception) {
+                    return response()->format(422, 'token_failure');
+                }
+            }
         }
         return $next($request);
     }
