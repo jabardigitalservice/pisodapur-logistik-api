ae4feb1ef28df9bcb72b1f758ccce04fbbeb3bbf
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-11-06 14:02:10 +0700

    Penyesuaian alur API Notifikasi Perubahan Status Permohonan via WA

--- app/Http/Controllers/API/v1/ChangeStatusNotifyController.php
+++ app/Http/Controllers/API/v1/ChangeStatusNotifyController.php
@@ -3,21 +3,34 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\User;
+use App\Validation;
 use App\Http\Controllers\Controller;
 use App\Notifications\TestResult;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ChangeStatusNotifyController extends Controller
 {
     public function sendNotification(Request $request)
     {
-        $role = $request->input('roles', 'superadmin');
-        $users = User::where('roles', $role)->get();
-        foreach ($users as $user) {
-            $user->notify(new TestResult());
+        $param = [ 
+            'id' => 'required',
+            'url' => 'required',
+            'phase' => 'required',
+        ];
+        $response = Validation::validate($request, $param);
+        if ($response->getStatusCode() === 200) {
+            $notify = [];
+            $users = User::where('phase', $request->phase)->whereNotNull('handphone')->get();
+            foreach ($users as $user) {
+                $notify[] = $user->notify(new TestResult($request));
+            }
+            $responseData = [
+                'request' => $request->all(),
+                'users' => $users,
+                'notify' => $notify,
+            ];
+            $response = response()->format(200, 'success', $responseData);
         }
-
-        return response()->json(['message' => 'OK']);
+        return $response;
     }
 }
--- app/Notifications/TestResult.php
+++ app/Notifications/TestResult.php
@@ -6,19 +6,25 @@ use App\Channels\SmsChannel;
 use App\Channels\WhatsappChannel;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
+use Illuminate\Http\Request;
 
 class TestResult extends Notification
 {
     use Queueable;
 
+    public $link;
+    public $id;
+    public $phase;
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Request $request)
     {
-        //
+        $this->id = $request->id;
+        $this->phase = $request->phase;
+        $this->link = $request->url . '/alat-kesehatan/detail/' . $this->id;
     }
 
     /**
@@ -29,18 +35,35 @@ class TestResult extends Notification
      */
     public function via($notifiable)
     {
-        return [WhatsappChannel::class, SmsChannel::class];
+        return [WhatsappChannel::class];
     }
 
     public function toSms($notifiable)
     {
-        $message  = 'Sampurasun, hasil Tes COVID Anda sudah keluar. ';
+        $message = $this->setMessage();
         return $message;
     }
 
     public function toWhatsapp($notifiable)
     {
-        $message = 'Sampurasun, hasil Tes COVID-19 Anda sudah keluar. ';
+        $message = $this->setMessage();
+        return $message;
+    }
+
+    public function setMessage()
+    {
+        $message = '';
+        switch ($this->phase) {
+            case 'rekomendasi':
+                $message = 'Permohonan dengan kode: ' . $this->id . ' sudah dilakukan verifikasi administrasi dan perlu dilakukan rekomendasi salur. Berikut link permohonan yang perlu dilakukan rekomendasi salur ' . $this->link;
+                break;
+            case 'realisasi':
+                $message = 'Permohonan dengan kode: ' . $this->id . ' sudah dilakukan rekomendasi salur dan perlu dilakukan realisasi salur. Berikut link permohonan yang perlu dilakukan realisasi salur ' . $this->link;
+                break;
+            default:
+                $message = 'Permohonan dengan kode: ' . $this->id . ' sudah diterima mohon ditindaklanjuti dengan melakukan verifikasi. Berikut link permohonan yang perlu diverifikasi ' . $this->link;
+                break;
+        }
         return $message;
     }
 
