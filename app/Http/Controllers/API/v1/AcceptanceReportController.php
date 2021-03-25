04076f8b4cd5e90b794951ff12234448fb77ac0e
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-03-26 04:32:49 +0700

    Mengubah `$request->filled()` ke `$request->has()`

--- app/Http/Controllers/API/v1/AcceptanceReportController.php
+++ app/Http/Controllers/API/v1/AcceptanceReportController.php
@@ -22,11 +22,11 @@ class AcceptanceReportController extends Controller
      */
     public function index(Request $request)
     {
-        $request->start_date = $request->filled('start_date') ? $request->input('start_date') . ' 00:00:00' : '2020-01-01 00:00:00';
-        $request->end_date = $request->filled('end_date') ? $request->input('end_date') . ' 23:59:59' : date('Y-m-d H:i:s');
+        $request->start_date = $request->has('start_date') ? $request->input('start_date') . ' 00:00:00' : '2020-01-01 00:00:00';
+        $request->end_date = $request->has('end_date') ? $request->input('end_date') . ' 23:59:59' : date('Y-m-d H:i:s');
 
         $limit = $request->input('limit', 10);
-        $sort = $request->filled('sort') ? ['agency_name ' . $request->input('sort') . ', ', 'updated_at DESC'] : ['updated_at DESC, ', 'agency_name ASC'];
+        $sort = $request->has('sort') ? ['agency_name ' . $request->input('sort') . ', ', 'updated_at DESC'] : ['updated_at DESC, ', 'agency_name ASC'];
         $data = \App\Agency::getList($request, false);
         $data = $data->orderByRaw(implode($sort))->paginate($limit);
         return response()->format(Response::HTTP_OK, 'success', $data);
