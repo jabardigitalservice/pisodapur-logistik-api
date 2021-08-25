656c0a194468291a271d698972f9a5ece1a80cc3
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-08-26 03:39:52 +0700

    Integrated to Poslog when finished request phase

--- app/LogisticRequest.php
+++ app/LogisticRequest.php
@@ -246,6 +246,9 @@ class LogisticRequest extends Model
         $param['step'] = 'finalized';
         $param['phase'] = 'final';
         $response = self::getResponseApproval($request, $param);
+        if ($response->getStatusCode() == Response::HTTP_OK) {
+            $response = WmsJabar::sendPing();
+        }
         return $response;
     }
 
