1b47aca855d7aac5d4a97780adcd9e05e7dd548b
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-09-02 10:42:55 +0700

    Update LogisticRequest.php

--- app/LogisticRequest.php
+++ app/LogisticRequest.php
@@ -247,9 +247,9 @@ class LogisticRequest extends Model
         $param['phase'] = 'final';
         $response = self::getResponseApproval($request, $param);
         // Handling Poslog Integration
-        // if ($response->getStatusCode() == Response::HTTP_OK) {
-        //    $response = WmsJabar::sendPing();
-        // }
+        if ($response->getStatusCode() == Response::HTTP_OK) {
+            $response = WmsJabar::sendPing();
+        }
         return $response;
     }
 
