b182d03e646680707cf949b5c96a8c42c4df8778
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-10-13 11:15:31 +0700

    Errorfix API Verification Code Registration

--- app/Http/Controllers/API/v1/LogisticVerificationController.php
+++ app/Http/Controllers/API/v1/LogisticVerificationController.php
@@ -9,49 +9,46 @@ use App\Validation;
 use App\AcceptanceReport;
 use App\LogisticVerification;
 use App\Applicant;
+use App\Http\Requests\LogisticVerification\VerificationRegistrationRequest;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TokenEmailNotification;
 use Carbon\Carbon;
 
 class LogisticVerificationController extends Controller
 {
-    public function verificationRegistration(Request $request)
+    public function verificationRegistration(VerificationRegistrationRequest $request)
     {
-        $param['register_id'] = 'required|numeric';
-        $message = 'success';
-        $response = Validation::validate($request, $param);
-        $logisticVerification = [];
-        abort_if($response->getStatusCode() != Response::HTTP_OK, $response);
         $findReport = AcceptanceReport::where('agency_id', $request->register_id)->first();
-        abort_if($findReport, Response::HTTP_UNPROCESSABLE_ENTITY, 'Permohonan dengan kode ' . optional($findReport)->agency_id . ' sudah dilaporkan pada tanggal ' . Carbon::parse(optional($findReport)->date)->format('d-m-Y') . ' oleh ' . optional($findReport)->fullname . '. Terima kasih sudah melaporkan penerimaan barang');
-        $request->id = $request->register_id;
+
+        if ($findReport) {
+            return response()->format(Response::HTTP_UNPROCESSABLE_ENTITY, 'Permohonan dengan kode ' . optional($findReport)->agency_id . ' sudah dilaporkan pada tanggal ' . Carbon::parse(optional($findReport)->date)->format('d-m-Y') . ' oleh ' . optional($findReport)->fullname . '. Terima kasih sudah melaporkan penerimaan barang');
+        }
+
         try {
-            $applicant = Applicant::where('agency_id', $request->id)->firstOrFail();
-            $logisticVerification = LogisticVerification::firstOrCreate(['agency_id' => $request->id], ['email' => $applicant->email]);
+            $message = 'success';
+            $applicant = Applicant::where('agency_id', $request->register_id)->firstOrFail();
+            $logisticVerification = LogisticVerification::firstOrCreate(['agency_id' => $request->register_id], ['email' => $applicant->email]);
             if ($request->has('reset')) {
                 $message = 'Kode Verifikasi yang baru telah dikirim ulang ke email Anda.';
                 $logisticVerification->expired_at = date('Y-m-d H:i:s', strtotime('-1 days'));
             }
-            $response = response()->format(Response::HTTP_OK, $message, $logisticVerification);
+            $logisticVerification = $this->sendEmailCondition($logisticVerification);
+            return response()->format(Response::HTTP_OK, $message, $logisticVerification);
         } catch (\Exception $exception) {
-            $response = response()->format(Response::HTTP_UNPROCESSABLE_ENTITY, 'Permohonan dengan Kode Permohonan ' . $request->id . ' tidak ditemukan.');
+            return response()->format(Response::HTTP_UNPROCESSABLE_ENTITY, 'Permohonan dengan Kode Permohonan ' . $request->register_id . ' tidak ditemukan.');
         }
-        $logisticVerification = $this->sendEmailCondition($logisticVerification, $response);
-        return $response;
     }
 
-    public function sendEmailCondition($logisticVerification, $response)
+    public function sendEmailCondition($logisticVerification)
     {
-        if ($response->getStatusCode() === Response::HTTP_OK) {
-            if ($logisticVerification->expired_at <= date('Y-m-d H:i:s')) {
-                // reset token
-                $token = rand(10000, 99999);
-                $logisticVerification->token = $token;
-                $logisticVerification->expired_at = date('Y-m-d H:i:s', strtotime('+1 days'));
-                $logisticVerification->save();
-                // send email
-                Mail::to($logisticVerification->email)->send(new TokenEmailNotification($token));
-            }
+        if ($logisticVerification->expired_at <= date('Y-m-d H:i:s')) {
+            // reset token
+            $token = rand(10000, 99999);
+            $logisticVerification->token = $token;
+            $logisticVerification->expired_at = date('Y-m-d H:i:s', strtotime('+1 days'));
+            $logisticVerification->save();
+            // send email
+            Mail::to($logisticVerification->email)->send(new TokenEmailNotification($token));
         }
         return $logisticVerification;
     }
