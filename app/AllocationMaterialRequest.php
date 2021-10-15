bdb16bcbf831d3862694790ce6c5b6a43e363652
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-10-15 11:58:10 +0700

    API Import Allocation Vaccine Data

--- app/AllocationMaterialRequest.php
+++ app/AllocationMaterialRequest.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AllocationMaterialRequest extends Model
 {
-    //
+    protected $fillable = [
+        'allocation_request_id',
+        'allocation_distribution_request_id',
+        'matg_id',
+        'material_id',
+        'material_name',
+        'qty',
+        'UoM'
+    ];
 }
