abbbb4e5c861359ee28cb75efc8c58b5f6ed55ac
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-10-21 15:02:51 +0700

    API Get Allocation Distribution Request by Allocation Request ID

---
+++ app/Http/Controllers/API/v1/AllocationDistributionVaccineRequestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\AllocationDistributionRequest;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AllocationRequest\GetAllocationDistributionVaccineRequest;
+
+class AllocationDistributionVaccineRequestController extends Controller
+{
+    public function index(GetAllocationDistributionVaccineRequest $request)
+    {
+        $limit = $request->input('limit', 10);
+        $data = AllocationDistributionRequest::where('allocation_request_id', $request->input('allocation_request_id'))
+            ->with(['allocationMaterialRequests'])
+            ->paginate($limit);
+        return response()->format(Response::HTTP_OK, 'success', $data);
+    }
+}
