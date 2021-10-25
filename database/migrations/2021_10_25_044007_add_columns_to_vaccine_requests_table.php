ae6fc7ac593dc6817f301ac901894cd6dee59961
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-10-25 12:53:48 +0700

    add is_completed and is_urgency

---
+++ database/migrations/2021_10_25_044007_add_columns_to_vaccine_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToVaccineRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vaccine_requests', function (Blueprint $table) {
+            $table->boolean('is_completed')->default(0);
+            $table->boolean('is_urgency')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vaccine_requests', function (Blueprint $table) {
+            //
+        });
+    }
+}
