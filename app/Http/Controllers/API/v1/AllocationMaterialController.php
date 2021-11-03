3c37ed4ebd3b600e7e54a71987c49ef32ad078f2
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-11-03 14:32:38 +0700

    Allocation Material List Filter by  Material Name

--- app/Http/Controllers/API/v1/AllocationMaterialController.php
+++ app/Http/Controllers/API/v1/AllocationMaterialController.php
@@ -6,15 +6,20 @@ use App\AllocationMaterial;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AllocationRequest\GetAllocationMaterialRequest;
 
 class AllocationMaterialController extends Controller
 {
-    public function index(Request $request)
+    public function index(GetAllocationMaterialRequest $request)
     {
         $isPaginated = $request->input('is_paginated', 0);
         $type = $request->input('type', 'vaccine');
         $limit = $request->input('limit', 10);
         $data = AllocationMaterial::where('type', $type)
+                ->when($request->input('material_name'), function ($query) use ($request) {
+                    $query->where('material_name', 'LIKE', "%{$request->input('material_name')}%");
+                });
+
         $data = $isPaginated ? $data->get() : $data->paginate($limit);
         return response()->format(Response::HTTP_OK, 'success', $data);
     }
