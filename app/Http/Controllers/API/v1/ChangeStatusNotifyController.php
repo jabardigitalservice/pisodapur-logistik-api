3631c3e82f9248b438d1de5673b3d13536e60ad7
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-12-15 11:07:43 +0700

    ChangeStatusNotifyTest

--- app/Http/Controllers/API/v1/ChangeStatusNotifyController.php
+++ app/Http/Controllers/API/v1/ChangeStatusNotifyController.php
@@ -3,39 +3,31 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\User;
-use App\Validation;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ChangeStatusNotifyRequest;
 use App\Notifications\ChangeStatusNotification;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ChangeStatusNotifyController extends Controller
 {
-    public function sendNotification(Request $request)
+    public function sendNotification(ChangeStatusNotifyRequest $request)
     {
-        $param = [ 
-            'id' => 'required',
-            'url' => 'required',
-            'phase' => 'required',
+        $notify = [];
+        $users = User::where('phase', $request->phase)->where('handphone', '!=', '')->get();
+        $requiredData = [
+            'id' => $request->id,
+            'url' => $request->url,
+            'phase' => $request->phase,
         ];
-        $response = Validation::validate($request, $param);
-        if ($response->getStatusCode() === 200) {
-            $notify = [];
-            $users = User::where('phase', $request->phase)->where('handphone', '!=', '')->get();
-            $requiredData = [
-                'id' => $request->id,
-                'url' => $request->url,
-                'phase' => $request->phase,
-            ];
-            foreach ($users as $user) {
-                $notify[] = $user->notify(new ChangeStatusNotification($requiredData));
-            }
-            $responseData = [
-                'request' => $request->all(),
-                'users' => $users,
-                'notify' => $notify,
-            ];
-            $response = response()->format(200, 'success', $responseData);
+        foreach ($users as $user) {
+            $notify[] = $user->notify(new ChangeStatusNotification($requiredData));
         }
+        $responseData = [
+            'request' => $request->all(),
+            'users' => $users,
+            'notify' => $notify,
+        ];
+        $response = response()->format(Response::HTTP_OK, 'success', $responseData);
         return $response;
     }
 }
