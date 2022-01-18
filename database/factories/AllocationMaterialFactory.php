2790b3ac091497b51a5e3a489843a112ceec8305
brin <budiaramdhanrindi@gmail.com>
2022-01-18 15:02:24 +0700

    Fixing AllocationMaterialFactory

--- database/factories/AllocationMaterialFactory.php
+++ database/factories/AllocationMaterialFactory.php
@@ -19,6 +19,11 @@ $factory->define(App\AllocationMaterial::class, function (Faker $faker) {
         'material_id' => $faker->numerify('MAT-' . substr($faker->name, 1) . '##' . substr($faker->name, 3) . '##'),
         'material_name' => $faker->company,
         'type' => 'vaccine',
-        'UoM' => 'PCS'
+        'UoM' => $faker->stateAbbr(),
+        'soh_location' => $faker->citySuffix,
+        'soh_location_name' => $faker->city,
+        'stock_ok' => rand(0, 1000),
+        'stock_nok' => rand(0, 1000),
+        'booked_stock' => rand(0, 1000)
     ];
 });
