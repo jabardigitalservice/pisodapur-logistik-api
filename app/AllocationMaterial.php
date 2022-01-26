b527598fb45413d4f1000bbbd12d888bf0e568d8
brin <budiaramdhanrindi@gmail.com>
2022-01-27 01:13:43 +0700

    API Get Allocation Material - get Current Stock

--- app/AllocationMaterial.php
+++ app/AllocationMaterial.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class AllocationMaterial extends Model
 {
-    //
+    protected $appends = ['current_stock', 'current_stock_formatted'];
+    public function getCurrentStockAttribute()
+    {
+        return $this->stock_ok - $this->booked_stock;
+    }
+
+    public function getCurrentStockFormattedAttribute()
+    {
+        return number_format($this->getCurrentStockAttribute(), 0, ',', '.');
+    }
 }
