1419674869e68c67b93f41bd0676bd30be43e097
brin <budiaramdhanrindi@gmail.com>
2022-02-25 09:20:33 +0700

    fixing Function rules has a Cognitive Complexity
    https://codeclimate.com/github/jabardigitalservice/pikobar-logistik-api/pull/377

--- app/Http/Requests/StoreVaccineRequest.php
+++ app/Http/Requests/StoreVaccineRequest.php
@@ -7,6 +7,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreVaccineRequest extends FormRequest
 {
+    const RULES = [
+        'master_faskes_id' => 'required|numeric|exists:master_faskes,id',
+        'agency_type' => 'required|numeric|exists:master_faskes_types,id',
+        'agency_name' => 'required|string',
+        'location_district_code' => 'required|string|exists:districtcities,kemendagri_kabupaten_kode',
+        'location_subdistrict_code' => 'required|string|exists:subdistricts,kemendagri_kecamatan_kode',
+        'location_village_code' => 'required|string|exists:villages,kemendagri_desa_kode',
+        'applicant_name' => 'required|string',
+        'primary_phone_number' => 'required|numeric',
+        'letter_file' => 'required|mimes:jpeg,jpg,png,pdf|max:10240',
+        'applicant_file' => 'mimes:jpeg,jpg,png,pdf|max:10240',
+        'is_letter_file_final' => 'required|boolean',
+        'application_letter_number' => 'required|string',
+    ];
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -24,21 +39,15 @@ class StoreVaccineRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'master_faskes_id' => 'required|numeric|exists:master_faskes,id',
-            'agency_type' => 'required|numeric|exists:master_faskes_types,id',
-            'agency_name' => 'required|string',
-            'location_district_code' => 'required|string|exists:districtcities,kemendagri_kabupaten_kode',
-            'location_subdistrict_code' => 'required|string|exists:subdistricts,kemendagri_kecamatan_kode',
-            'location_village_code' => 'required|string|exists:villages,kemendagri_desa_kode',
-            'applicant_name' => 'required|string',
-            'primary_phone_number' => 'required|numeric',
+        $rules = self::RULES;
+        $rules += [
             'logistic_request' => [
                 'required',
                 'json',
                 function ($attribute, $value, $fail) {
                     foreach (json_decode($value, true) as $key => $param) {
                         if (!is_numeric($param['product_id'])) {
+                            $fail('logistic_request.product_id is required.');
                         }
 
                         if (!is_numeric($param['quantity'])) {
@@ -55,10 +64,8 @@ class StoreVaccineRequest extends FormRequest
                     }
                 },
             ],
-            'letter_file' => 'required|mimes:jpeg,jpg,png,pdf|max:10240',
-            'applicant_file' => 'mimes:jpeg,jpg,png,pdf|max:10240',
-            'is_letter_file_final' => 'required|boolean',
-            'application_letter_number' => 'required|string'
         ];
+
+        return $rules;
     }
 }
