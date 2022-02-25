b579d56fb3de49eca544766ac8ad9555a45c40bc
brin <budiaramdhanrindi@gmail.com>
2022-02-25 10:56:00 +0700

    scopeFilter has 32 lines of code
    Method scopeFilter has 32 lines of code (exceeds 25 allowed).

--- app/Models/Vaccine/VaccineRequest.php
+++ app/Models/Vaccine/VaccineRequest.php
@@ -85,37 +85,46 @@ class VaccineRequest extends Model
 
     public function scopeFilter($query, $request)
     {
-        $query->when($request->input('status'), function ($query) use ($request) {
-            $query->when($request->input('status') == VaccineRequestStatusEnum::rejected(), function ($query) use ($request) {
-                $query->whereIn('status', [VaccineRequestStatusEnum::verification_rejected(), VaccineRequestStatusEnum::approval_rejected()]);
-            }, function ($query) use ($request) {
-                $query->where('status', $request->input('status'));
+        $query
+            ->when($request->input('status'), function ($query) use ($request) {
+                $query->when($request->input('status') == VaccineRequestStatusEnum::rejected(), function ($query) use ($request) {
+                    $query->whereIn('status', [VaccineRequestStatusEnum::verification_rejected(), VaccineRequestStatusEnum::approval_rejected()]);
+                }, function ($query) use ($request) {
+                    $query->where('status', $request->input('status'));
+                });
+            })
+            ->when($request->input('start_date') && $request->input('end_date'), function ($query) use ($request) {
+                $start_date = $request->input('start_date') . ' 00:00:00';
+                $end_date = $request->input('end_date') . ' 23:59:59';
+                $query->whereBetween('created_at', [$start_date, $end_date]);
+            })
+            ->when($request->input('city_id'), function ($query) use ($request) {
+                $query->where('agency_city_id', $request->input('city_id'));
+            })
+            ->when($request->has('is_completed'), function ($query) use ($request) {
+                $query->where('is_completed', $request->input('is_completed'));
+            })
+            ->when($request->has('is_urgency'), function ($query) use ($request) {
+                $query->where('is_urgency', $request->input('is_urgency'));
+            })
+            ->when($request->input('faskes_type'), function ($query) use ($request) {
+                $query->where('agency_type_id', $request->input('faskes_type'));
             });
-        })
-        ->when($request->input('start_date') && $request->input('end_date'), function ($query) use ($request) {
-            $start_date = $request->input('start_date') . ' 00:00:00';
-            $end_date = $request->input('end_date') . ' 23:59:59';
-            $query->whereBetween('created_at', [$start_date, $end_date]);
-        })
-        ->when($request->input('city_id'), function ($query) use ($request) {
-            $query->where('agency_city_id', $request->input('city_id'));
-        })
-        ->when($request->has('is_completed'), function ($query) use ($request) {
-            $query->where('is_completed', $request->input('is_completed'));
-        })
-        ->when($request->has('is_urgency'), function ($query) use ($request) {
-            $query->where('is_urgency', $request->input('is_urgency'));
-        })
-        ->when($request->input('faskes_type'), function ($query) use ($request) {
-            $query->where('agency_type_id', $request->input('faskes_type'));
-        });
 
+        $query->whereHasMasterFaskes($request);
+        return $query;
+    }
+
+    public function scopeWhereHasMasterFaskes($query, $request)
+    {
         $query->whereHas('masterFaskes', function ($query) use ($request) {
-            $query->when($request->has('is_reference'), function ($query) use ($request) {
-                $query->where('is_reference', $request->input('is_reference'));
-            })->when($request->input('search'), function ($query) use ($request) {
-                $query->where('nama_faskes', 'like', '%' . $request->input('search') . '%');
-            });
+            $query
+                ->when($request->has('is_reference'), function ($query) use ($request) {
+                    $query->where('is_reference', $request->input('is_reference'));
+                })
+                ->when($request->input('search'), function ($query) use ($request) {
+                    $query->where('nama_faskes', 'like', '%' . $request->input('search') . '%');
+                });
         });
         return $query;
     }
