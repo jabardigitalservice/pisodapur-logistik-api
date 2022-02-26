999e0039a8b848cec65af697b74690df154d6c8d
brin <budiaramdhanrindi@gmail.com>
2022-02-26 17:27:18 +0700

    Letter Model - Remove unused relational function

--- app/Letter.php
+++ app/Letter.php
@@ -15,11 +15,6 @@ class Letter extends Model
         'letter'
     ];
 
-    public function agency()
-    {
-        return $this->belongsToOne('App\Agency', 'id', 'agency_id');
-    }
-
     public function getLetterAttribute($value)
     {
         $data = FileUpload::find($value);
