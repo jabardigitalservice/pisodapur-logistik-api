b486d6dc6716955e3a098e27f8adbdd94db4b126
brin <budiaramdhanrindi@gmail.com>
2022-05-19 08:21:49 +0700

    ngajlok

--- app/Http/Requests/LogisticRealizationItem/StoreRequest.php
+++ app/Http/Requests/LogisticRealizationItem/StoreRequest.php
@@ -6,7 +6,7 @@ use App\LogisticRealizationItems;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-    class StoreRequest extends FormRequest
+class StoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
