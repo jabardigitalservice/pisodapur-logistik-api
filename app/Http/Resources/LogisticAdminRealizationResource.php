d11336a20486fa6368a7e8f472b1e588e145b0f7
Imam Fahmi F <63537236+imamdev93@users.noreply.github.com>
2022-10-19 10:33:50 +0700

    change limit and add if condition product is null

--- app/Http/Resources/LogisticAdminRealizationResource.php
+++ app/Http/Resources/LogisticAdminRealizationResource.php
@@ -17,7 +17,7 @@ class LogisticAdminRealizationResource extends JsonResource
 
     function __construct($data, $request)
     {
-        $this->limit = $request->input('limit', 3);
+        $this->limit = $request->input('limit', 10);
         $this->data = $data;
     }
     /**
@@ -35,15 +35,17 @@ class LogisticAdminRealizationResource extends JsonResource
     {
         $data = array();
 
-        $recommendation = $this->data
-            ->joinNeed('recommendation')
-            ->select($this->selectRecommendation())
-            ->paginate($this->limit);
-
         $realization = $this->data
             ->joinNeed('realization')
             ->select($this->selectRealization())
             ->paginate($this->limit);
+
+        $recommendation = $this->data
+            ->joinNeed('recommendation')
+            ->select($this->selectRecommendation())
+            ->whereNotNull('logistic_realization_items.product_id')
+            ->paginate($this->limit);
+
 
         array_push($data, $this->getDataTransform($recommendation, 'recommendation', true));
         array_push($data, $this->getDataTransform($realization, 'realiazation', true));
