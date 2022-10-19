4af5d0c062e833d3274c654012e7c18c0a31a801
Imam Fahmi F <63537236+imamdev93@users.noreply.github.com>
2022-10-19 19:58:39 +0700

    change transform data

--- app/Traits/TransformTrait.php
+++ app/Traits/TransformTrait.php
@@ -2,47 +2,74 @@
 
 namespace App\Traits;
 
-use App\LogisticRealizationItems;
-use App\Needs;
-use App\Product;
-
 trait TransformTrait
 {
-    public function getDataTransform($data, $phase = 'administration', $isSalur = false)
+    public function getDataTransform($data, $phase = 'administration')
     {
-        $data->getCollection()->transform(function ($item) use ($phase, $isSalur) {
+        $data->getCollection()->transform(function ($item) use ($phase) {
             $item->status = !$item->status ? 'not_approved' : $item->status;
+            $item->final_status = !$item->final_status ? 'not_approved' : $item->final_status;
 
-            if ($item->status == 'not_approved' && $phase != 'administration') {
-                $data = $this->getItems($item, $phase, $isSalur);
-                $item->product_id = $phase == 'recommendation' ? ($data->product->id) ?? null : $data->product_id;
-                $item->product_name = $phase == 'recommendation' ? ($data->product->name) ?? null : $data->product_name;
-                $item->unit = $phase == 'recommendation' ? ($data->masterUnit->unit) ?? null : $data->realization_unit;
-                $item->quantity = null;
+            switch ($phase) {
+                case 'recommendation':
+                    $response = [
+                        'product_id' => $item->recommendation_product_id,
+                        'product_name' => $item->recommendation_product_name,
+                        'unit' => $item->recommendation_unit,
+                        'date' => $item->recommendation_date,
+                        'quantity' => $item->recommendation_quantity,
+                        'status' => $item->status,
+                    ];
+
+                    if ($item->status == 'not_approved') {
+                        $response = [
+                            'product_id' => $item->recommendation_product_id ?? $item->product_id,
+                            'product_name' => $item->recommendation_product_name ?? $item->product_name,
+                            'unit' => $item->recommendation_unit ?? $item->unit,
+                            'date' => null,
+                            'quantity' => null,
+                            'status' => $item->status,
+                        ];
+                    }
+                    break;
+                case 'realization':
+                    $response = [
+                        'product_id' => $item->final_product_id,
+                        'product_name' => $item->final_product_name,
+                        'unit' => $item->final_unit,
+                        'date' => $item->final_date,
+                        'quantity' => $item->final_quantity,
+                        'status' => $item->final_status,
+                    ];
+
+                    if ($item->final_status == 'not_approved') {
+                        $response = [
+                            'product_id' => $item->final_product_id ?? $item->recommendation_product_id,
+                            'product_name' => $item->final_product_name ?? $item->recommendation_product_name,
+                            'unit' => $item->final_unit ?? $item->recommendation_unit,
+                            'date' => $item->final_date ?? $item->recommendation_date,
+                            'quantity' => null,
+                            'status' => $item->final_status ?? $item->status,
+                        ];
+                    }
+                    break;
+                default:
+                    $response = [
+                        'product_id' => $item->product_id,
+                        'product_name' => $item->product_name,
+                        'unit' => $item->unit,
+                        'date' => $item->date,
+                        'quantity' => $item->quantity,
+                        'status' => $item->status
+                    ];
+                    break;
             }
-
-            if ($item->status == 'not_approved' && $isSalur) {
-                $data = $this->getItems($item, $phase, $isSalur);
-                $item->product_id = $data->product->id ?? $data->product_id;
-                $item->product_name = $data->product->name ?? $data->product_name;
-                $item->unit = $data->realization_unit;
-                $item->quantity = null;
-            }
-
-
-            return $item;
+            return $response + [
+                'brand' => $item->brand,
+                'category' => $item->category,
+                'request_quantity' => $item->request_quantity,
+            ];
         });
-
-        return $data;
-    }
-
-    public function getItems($item, $phase, $isSalur)
-    {
-        $data = LogisticRealizationItems::find($item->id);
-
-        if ($phase == 'recommendation' && !$isSalur) {
-            $data = Needs::find($item->need_id);
-        }
 
         return $data;
     }
