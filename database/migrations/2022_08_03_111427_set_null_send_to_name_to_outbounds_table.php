13294df039a21897067af402b5e8739b57c864ad
Imam Fahmi F <63537236+imamdev93@users.noreply.github.com>
2022-11-07 10:33:23 +0700

    change migration rollback

--- database/migrations/2022_08_03_111427_set_null_send_to_name_to_outbounds_table.php
+++ database/migrations/2022_08_03_111427_set_null_send_to_name_to_outbounds_table.php
@@ -36,7 +36,17 @@ class SetNullSendToNameToOutboundsTable extends Migration
     public function down()
     {
         Schema::table('outbounds', function (Blueprint $table) {
-            //
+            $table->date('lo_date')->change();
+            $table->string('lo_desc')->change();
+            $table->string('lo_cb')->change();
+            $table->string('lo_issued_by')->change();
+            $table->dateTime('lo_ct')->change();
+            $table->string('send_to_id', 20)->change();
+            $table->string('send_to_name')->change();
+            $table->string('city_id', 5)->change();
+            $table->string('send_to_city')->change();
+            $table->string('lo_location', 25)->change();
+            $table->string('whs_name')->change();
         });
     }
 }
